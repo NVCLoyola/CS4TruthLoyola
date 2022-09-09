dbc44289399d39637d1614bf225a97f371999107
NVCLoyola <95131093+NVCLoyola@users.noreply.github.com>
2022-09-09 08:49:32 +0800

    Add files via upload

---
+++ Q1Ex1/truth/loyola/Ex01TruthLoyola.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex01.truth.loyola;
+
+/**
+ *
+ * @author TRUTH
+ */
+public class Ex01TruthLoyola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String child1 = "Vince Loyola";
+        String favsnack1 = "Spaghetti";
+        int age1 = 14;
+        String child2 = "Edward Loyola";
+        String favsnack2 = "Pansit";
+        int age2 = 13;
+        String child3 = "Nicole Loyola";
+        String favsnack3 = "French Fries";
+        int age3 = 11;
+        
+        System.out.println("Child 1" + "\n" + "Name: " + child1 + "\n" + "Favorite Snack: " + favsnack1 + "\n" + "Age: " + age1 + "\n");    // TODO code application logic here
+        System.out.println("Child 2" + "\n" + "Name: " + child2 + "\n" + "Favorite Snack: " + favsnack2 + "\n" + "Age: " + age2 + "\n");
+        System.out.println("Child 3" + "\n" + "Name: " + child3 + "\n" + "Favorite Snack: " + favsnack3 + "\n" + "Age: " + age3 + "\n");
+    }
+    
+}
